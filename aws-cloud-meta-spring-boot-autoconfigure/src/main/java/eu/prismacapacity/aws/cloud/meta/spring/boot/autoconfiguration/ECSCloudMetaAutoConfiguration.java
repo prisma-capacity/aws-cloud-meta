41edb4d412bead1b9c7e374eeb65a43181eeebf7
Mattia Brescia <9354343-mattiabrescia@users.noreply.gitlab.com>
2022-11-21 15:46:40 +0100

    #543: new auto config annotation and factory file

--- aws-cloud-meta-spring-boot-autoconfigure/src/main/java/eu/prismacapacity/aws/cloud/meta/spring/boot/autoconfiguration/ECSCloudMetaAutoConfiguration.java
+++ aws-cloud-meta-spring-boot-autoconfigure/src/main/java/eu/prismacapacity/aws/cloud/meta/spring/boot/autoconfiguration/ECSCloudMetaAutoConfiguration.java
@@ -15,14 +15,14 @@
  */
 package eu.prismacapacity.aws.cloud.meta.spring.boot.autoconfiguration;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import eu.prismacapacity.aws.cloud.meta.spring.ecs.ECSCloudMetaConfiguration;
 
-@Configuration
+@AutoConfiguration
 @Import(ECSCloudMetaConfiguration.class)
 @ConditionalOnClass(ECSCloudMetaConfiguration.class)
 @ConditionalOnProperty(name = "aws.cloud.meta.enabled", havingValue = "true")
